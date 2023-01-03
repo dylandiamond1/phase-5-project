de6572abd789cad87d8647d2965f1aecb94b1b61
dylan <dylandiamond34@gmail.com>
2023-01-03 11:09:56 -0700

    added css

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -53,9 +53,9 @@ const handleLogOut = () => {
           <div className="navbar-start">
             <div className="dropdown">
               <label tabIndex={0} className="btn btn-ghost btn-circle">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                   </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52" style={{background: "white"}}>
                           <li><Link to='AboutUs'>About Us</Link></li>
                           <li><Link to='OurProcess'>Our Process</Link></li>
                           <li><Link to='/PastProjects'>Our Houses</Link></li>
@@ -73,7 +73,7 @@ const handleLogOut = () => {
         <img src="https://placeimg.com/192/192/people" />
         </div>
       </label>
-      <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52" style={{background: "white"}}>
         <li><button onClick={handleLogOut}>Log Out</button></li>
         <li><Link to='/login'>Login</Link></li>
         <li><Link to='/users/new'>Sign Up</Link></li>
